a190badaf1b7512519c3caadae6ebc830d588732
hezhe <hezhe8852@126.com>
2021-06-25 11:07:21 +0800

    emailService

---
+++ src/test/java/com/three/email/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
